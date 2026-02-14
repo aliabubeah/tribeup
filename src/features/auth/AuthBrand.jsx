5e243b00fa2f5374da29e0776b89783221f9de58
Ali Mohamed <aliabubeah832@gmail.com>
2026-02-14 19:54:36 +0200

    bug fix on deploy

--- src/features/auth/AuthBrand.jsx
+++ src/features/auth/AuthBrand.jsx
@@ -3,7 +3,7 @@ function AuthBrand() {
         <div className="w-full">
             <img
                 className="h-28 w-28 justify-self-center rounded-full"
-                src="../../../public/tribeup.png"
+                src="/tribeup.png"
                 alt=""
             />
             <h1 className="text-center text-3xl font-bold tracking-wide text-tribe-600 lg:text-4xl">
