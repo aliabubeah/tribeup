fc402a31e3759fc8927fdddccc4bdb44f6b7ac5f
Ali Mohamed <aliabubeah832@gmail.com>
2026-02-21 18:53:24 +0200

    added rotation to the logo on splash screen

--- src/ui/SplashScreen.jsx
+++ src/ui/SplashScreen.jsx
@@ -2,7 +2,14 @@ function SplashScreen() {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center bg-neutral-50 p-5">
             <div className="flex grow items-center justify-center">
-                <img src="/tribeup.png" className="h-64 w-64 rounded-full" />
+                <img
+                    src="/السهم.png"
+                    className="relative h-16 w-16 rounded-full"
+                />
+                <img
+                    src="/الشكل.png"
+                    className="absolute h-64 w-64 animate-spin-slow rounded-full"
+                />
             </div>
             <h1 className="text-3xl font-semibold text-tribe-500">TribeUp</h1>
         </div>
